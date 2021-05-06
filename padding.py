498d297e0bdb545061a8707ed400503a71b3090e
Yuyyin <a01706515@itesm.mx>
2021-05-06 12:43:55 -0500

    Documentación

--- padding.py
+++ padding.py
@@ -1,11 +1,12 @@
+#libreria para matrices "numpy"
 import numpy as np
 
+#Función del padding 
 def pading(image,pad):
 
-    irow,icol=image.shape
+    irow,icol=image.shape  #Tamaño de la matriz
 
-
-    output = np.zeros(image.shape)
+    output = np.zeros(image.shape) #Matriz de 0
     
     padded_image = np.zeros((irow + (2 * pad), icol + (2 * pad)))
     padded_image[pad:padded_image.shape[0] - pad, pad:padded_image.shape[1] - pad] = image
@@ -14,12 +15,13 @@ def pading(image,pad):
 
 if __name__ == '__main__':
 
+#Matriz de prueba para la función
     image = np.array([[1,3,43,1,2,3],
                     [7,0,0,9,1,12],
                     [0,1,2,16,17,20],
                     [0,0,0,7,100,22],
-                    [1,0,0,0,4,3]])
+                    ])
 
-    pad = 1
+    pad = 1 #Tamaño del pad
 
-pading(image,pad)
+pading(image,pad)  # Se llama la función
