72bdd0bf0bd6630c9ad06c5ad008c0fa628904bd
Yuyyin <a01706515@itesm.mx>
2021-05-06 14:03:51 -0500

    Ya compila

--- filtro.py
+++ filtro.py
@@ -15,7 +15,8 @@ def conv_helper(fragment, kernel):
     
     f_row, f_col = fragment.shape
     k_row, k_col = kernel.shape 
-    result = 0.0
+    result = 0
+
     for row in range(f_row):
         for col in range(f_col):
             result += fragment[row,col] *  kernel[row,col]
@@ -25,6 +26,13 @@ def convolution(image, kernel):
     """Aplica una convolucion sin padding (valida) de una dimesion 
     y devuelve la matriz resultante de la operación
     """
+#Se encuentra la dimencion de la imagen
+    if len(image.shape) == 3: #De 3 dimenciones
+        print("Dimenciones de imagen: {}".format(image.shape))
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) #Se cambia a dos dimenciones
+        print("Nuevas dimenciones: {}".format(image.shape))
+    else:
+        print("Dimenciones de imagen: {}".format(image.shape))
 
     image_row, image_col = image.shape #asigna alto y ancho de la imagen 
     kernel_row, kernel_col = kernel.shape #asigna alto y ancho del filtro
@@ -39,17 +47,19 @@ def convolution(image, kernel):
             output[row, col] = conv_helper(
                                 image[row:row + kernel_row, 
                                 col:col + kernel_col],kernel)
+    plt.imshow(output, cmap='gray')
+    plt.title("vale verga profe")
+    plt.show()
+    
+    return output
 
 if __name__ == '__main__':
 
-    image = cv2.imread("casita.jpg")
+    image = cv2.imread("index.jpg")
 
-    filter = np.array([[1/9,1/9,1/9],
-                    [1/9,1/9,1/9],
-                    [1/9,1/9,1/9]]) 
-plt.imshow(image, cmap='gray')
-plt.title('woow')
-plt.show()
+    filter = np.array([[2,-1,-1],
+                    [-1,2,-1],
+                    [-1,-1,2]]) 
 
 print("Filter: ")
 print(filter)
