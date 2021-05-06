a49e7a6e40d4294121f6d5c1b34c8c992144d633
Yuyyin <a01706515@itesm.mx>
2021-05-05 20:51:35 -0500

    Original modificado

--- convulaciones.py
+++ convulaciones.py
@@ -6,10 +6,10 @@ blog http://www.adeveloperdiary.com/data-science/computer-vision/how-to-implemen
 Modified by Uriel Sánchez
 """
 
-import numpy as np
-import cv2
-import matplotlib.pyplot as plt
- 
+import numpy as np   #numpy es una libreria de python
+import cv2           #import cv2 libreria de python
+import matplotlib.pyplot as plt #marplotlib.pyplot es otra de las librerias de python
+
 def conv_helper(fragment, kernel):
     """ multiplica 2 matices y devuelve su suma"""
     
@@ -28,17 +28,30 @@ def convolution(image, kernel):
 
     image_row, image_col = image.shape #asigna alto y ancho de la imagen 
     kernel_row, kernel_col = kernel.shape #asigna alto y ancho del filtro
-   
-    output = np.zeros(image.shape) #matriz donde guardo el resultado
-   
-    for row in range(image_row):
-        for col in range(image_col):
-                output[row, col] = conv_helper(
-                                    image[row:row + kernel_row, 
-                                    col:col + kernel_col],kernel)
-             
-    plt.imshow(output, cmap='gray')
-    plt.title("Output Image using {}X{} Kernel".format(kernel_row, kernel_col))
-    plt.show()
- 
-    return output
+    
+    output_x = (image_col - (kernel_col / 2) * 2) + 1 
+    output_y = (image_row - (kernel_row / 2) * 2) + 1 
+    
+    output = np.zeros([int(output_y), int(output_x)]) #matriz donde guardo el resultado
+
+    for row in range(int(output_y)):
+        for col in range(int(output_x)):
+            output[row, col] = conv_helper(
+                                image[row:row + kernel_row, 
+                                col:col + kernel_col],kernel)
+    print(output)
+
+if __name__ == '__main__':
+
+    image = np.array([[1,3,43,1,2,3],
+                    [7,0,0,9,1,12],
+                    [0,1,2,16,17,20],
+                    [0,0,0,7,100,22],
+                    [1,0,0,0,4,3]])
+
+    filter = np.array([[1,1,1],
+                    [0,0,0],
+                    [4,0,5]]) 
+                    
+
+convolution(image, filter)
